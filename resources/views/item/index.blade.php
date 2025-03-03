ebc82ddfabf03cb37aefd8711723cbf96929bfe4
s-lin96 <185015844+s-lin96@users.noreply.github.com>
2025-03-03 19:45:27 +0900

    250303/update:商品一覧のhtmlを修正

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -29,7 +29,7 @@
                     </div>
                 </div>
                 <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
+                    <table class="table table-hover text-nowrap text-center">
                         <thead>
                             <tr>
                                 <th>ID</th>
