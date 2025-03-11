27313396c54534e35c263c1bc4ca847035ae89ba
s-lin96 <185015844+s-lin96@users.noreply.github.com>
2025-03-11 15:21:42 +0900

    250311/update:同じプリフィックスのルートをグループ化

--- routes/web.php
+++ routes/web.php
@@ -21,32 +21,51 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// 管理者向・一般ユーザー共用
-Route::get('/items', [App\Http\Controllers\HomeController::class, 'showItemsTable'])->name('items.table.readonly');
-Route::get('/items/detail/{id}', [App\Http\Controllers\HomeController::class, 'showDetail'])->name('item.detail');
-Route::get('/items/search-item', [App\Http\Controllers\HomeController::class, 'searchItem'])->name('search.by.allusers');
+// 管理者・一般ユーザー共用
+Route::prefix('items')->group(function(){
+    // 商品一覧(削除済み含まない)
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'showItemsTable'])->name('items.table.readonly');
+    // 商品詳細
+    Route::get('/detail/{id}', [App\Http\Controllers\HomeController::class, 'showDetail'])->name('item.detail');
+    // 商品検索
+    Route::get('/search-item', [App\Http\Controllers\HomeController::class, 'searchItem'])->name('search.by.allusers');
+});
 
 // 管理者ユーザーのみがアクセスできるルート
 Route::middleware(['can:isAdmin'])->group(function(){
-    Route::get('/items/admin', [App\Http\Controllers\ItemController::class, 'index'])->name('items.table'); // 商品一覧(削除済み非表示)
-    Route::get('/items/admin/show-deleted', [App\Http\Controllers\ItemController::class, 'showDeleted'])->name('deleted.items.show'); // 商品一覧(削除済み表示)
-    Route::get('/items/admin/search-item', [App\Http\Controllers\ItemController::class, 'searchItem'])->name('search.by.admin');
+    Route::prefix('items/admin')->group(function(){
+        // 商品一覧(削除済み非表示)
+        Route::get('/', [App\Http\Controllers\ItemController::class, 'index'])->name('items.table');
+        // 商品一覧(削除済み表示)
+        Route::get('/show-deleted', [App\Http\Controllers\ItemController::class, 'showDeleted'])->name('deleted.items.show'); 
+        // 商品検索
+        Route::get('/search-item', [App\Http\Controllers\ItemController::class, 'searchItem'])->name('search.by.admin');
+        // 商品登録
+        Route::get('/add', [App\Http\Controllers\ItemController::class, 'create'])->name('item.create'); 
+        Route::post('/add', [App\Http\Controllers\ItemController::class, 'store'])->name('item.store');
+        // 商品編集
+        Route::get('/edit-detail/{id}', [App\Http\Controllers\ItemController::class, 'showItemEdit'])->name('item.edit'); 
+        Route::post('/edit-detail/{id}', [App\Http\Controllers\ItemController::class, 'updateItem'])->name('item.update');
+        // 商品削除(論理削除)
+        Route::get('/delete/{id}', [App\Http\Controllers\ItemController::class, 'delete'])->name('item.delete'); 
+        // 削除された商品復元
+        Route::get('/restore/{id}', [App\Http\Controllers\ItemController::class, 'restore'])->name('item.restore');
+        // 入出庫記録
+        Route::get('/record-stock/{id}', [App\Http\Controllers\ItemController::class, 'showStockRecord'])->name('stock.record');
+        Route::post('/record-stock/{id}', [App\Http\Controllers\ItemController::class, 'updateStock'])->name('stock.update');
+    });
 
-    Route::get('/items/admin/add', [App\Http\Controllers\ItemController::class, 'create'])->name('item.create'); // 商品登録フォーム
-    Route::post('/items/admin/add', [App\Http\Controllers\ItemController::class, 'store'])->name('item.store'); // 商品登録
-
-    Route::get('/items/admin/edit-detail/{id}', [App\Http\Controllers\ItemController::class, 'showItemEdit'])->name('item.edit'); // 商品編集フォーム
-    Route::post('/items/admin/edit-detail/{id}', [App\Http\Controllers\ItemController::class, 'updateItem'])->name('item.update'); // 商品編集
-
-    Route::get('/items/admin/delete/{id}', [App\Http\Controllers\ItemController::class, 'delete'])->name('item.delete'); // 商品論理削除
-    Route::get('/items/admin/restore/{id}', [App\Http\Controllers\ItemController::class, 'restore'])->name('item.restore');
-    Route::get('/items/admin/record-stock/{id}', [App\Http\Controllers\ItemController::class, 'showStockRecord'])->name('stock.record'); // 入出庫記録
-    Route::post('/items/admin/record-stock/{id}', [App\Http\Controllers\ItemController::class, 'updateStock'])->name('stock.update');
-
-    Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.table'); // ユーザー管理(削除済み非表示)
-    Route::get('/users/show-deleted', [App\Http\Controllers\UserController::class, 'showDeleted'])->name('deleted.users.show'); // ユーザー管理(削除済み表示)
-    Route::get('/users/edit-user/{id}', [App\Http\Controllers\UserController::class, 'showUserEdit'])->name('user.edit'); // ユーザー編集
-    Route::post('/users/edit-user/{id}', [App\Http\Controllers\UserController::class, 'updateUser'])->name('user.update');
-    Route::get('/users/delete/{id}', [App\Http\Controllers\UserController::class, 'delete'])->name('user.delete'); // ユーザー論理削除
-    Route::get('/users/restore/{id}', [App\Http\Controllers\UserController::class, 'restore'])->name('user.restore');
+    Route::prefix('users')->group(function(){
+        // ユーザー管理(削除済み非表示)
+        Route::get('/', [App\Http\Controllers\UserController::class, 'index'])->name('users.table');
+        // ユーザー管理(削除済み表示)
+        Route::get('/show-deleted', [App\Http\Controllers\UserController::class, 'showDeleted'])->name('deleted.users.show');
+        // ユーザー編集
+        Route::get('/edit-user/{id}', [App\Http\Controllers\UserController::class, 'showUserEdit'])->name('user.edit');
+        Route::post('/edit-user/{id}', [App\Http\Controllers\UserController::class, 'updateUser'])->name('user.update');
+        // ユーザー論理削除
+        Route::get('/delete/{id}', [App\Http\Controllers\UserController::class, 'delete'])->name('user.delete');
+        // 削除されたユーザー復元
+        Route::get('/restore/{id}', [App\Http\Controllers\UserController::class, 'restore'])->name('user.restore');
+    });
 });
