df555e8f2b3e9fb2166a44dfc718c4634279d931
s-lin96 <185015844+s-lin96@users.noreply.github.com>
2025-03-15 12:27:00 +0900

    250315/update:403、404エラー時のhtml、スタイルを修正

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,19 +1,23 @@
-@extends('adminlte::page')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@section('content')
-<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <div class="text-center">
-        <h1 class="display-1 mb-2">
-            <i class="fa fa-exclamation-circle" aria-hidden="true"></i> 403 Error
-        </h1>
-        <p class="lead">この操作は許可されていません。</p>
-        <a href="{{ route('home') }}" class="btn btn-primary" role="button">ホームへ</a>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    
+    <title>403 Error</title>
+</head>
+
+<body>
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="text-center">
+            <h1 class="display-1 mb-2">
+                <i class="fa fa-exclamation-circle" aria-hidden="true"></i> 403 Error
+            </h1>
+            <p class="lead">この操作は許可されていません。</p>
+        </div>
     </div>
-</div>
-@stop
-
-@section('css')
-@stop
-
-@section('js')
-@stop
+</body>
+</html>
