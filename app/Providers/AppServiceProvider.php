3f9ed4b33c7bc2d8f82ceaafb07223934a1939d3
s-lin96 <185015844+s-lin96@users.noreply.github.com>
2025-03-17 11:21:06 +0900

    250317/update:https通信するよう設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     {
         // Bootstrapを用いたページネーションビューを使用
         Paginator::useBootstrapFour();
+
+        // https通信を強制
+        if(App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
